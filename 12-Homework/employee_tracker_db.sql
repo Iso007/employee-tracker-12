60e6e326f828cfadb1e151d70a364373c86afa7f
Iso007 <isodiallo007@gmail.com>
2020-10-28 13:54:02 -0400

    incomplete, cant get it to work

---
+++ 12-Homework/employee_tracker_db.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE database employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT(10),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30),
+    department_id INT(10),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT(10) NULL,
+    manager_id INT(10) NULL,
+    PRIMARY KEY (id)
+);
